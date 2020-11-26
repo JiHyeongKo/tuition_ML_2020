37cb4f9701782794cdabd9f6a1b5187ca00ee6c9
JiHyeongKo <72496027+JiHyeongKo@users.noreply.github.com>
2020-11-26 22:01:57 +0900

    modify python and data

--- ML_고지형_1127/ML_고지형_1127/python/MLGraph_error_all_1127.py
+++ ML_고지형_1127/ML_고지형_1127/python/MLGraph_error_all_1127.py
@@ -2,20 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 MAX_COUNT = 1000	#epoch만큼 지정
-INSPECT_COUNT = 10	#inspect number만큼 지정
-COUNT = int(MAX_COUNT / INSPECT_COUNT)
 
-File = open('write_error1.txt', 'r', encoding = 'utf-8')
+File = open('write_error_all1.txt', 'r', encoding = 'utf-8')
 data = File.readline()
 
 epoch_array=[]
 error_array=[]
 
-for i in range(0, COUNT, 1):
+for i in range(0, MAX_COUNT, 1):
     epoch_array.append(0)
     error_array.append(0)
 
-for k in range(0, COUNT, 1):
+for k in range(0, MAX_COUNT, 1):
     lineBuffer = File.readline()
     line = (lineBuffer.rstrip())
     epoch_buffer = line[0:8]
@@ -23,13 +21,13 @@ for k in range(0, COUNT, 1):
     epoch_array[k] = epoch_buffer
     error_array[k] = error_buffer
 
-for i in range(1, COUNT, 1):
+for i in range(1, MAX_COUNT, 1):
     if ( epoch_array[i] == '') or (epoch_array[i] == '\n'):
         epoch_array[i]=epoch_array[i-1]
     if ( error_array[i] == '') or (error_array[i] == '\n'):
         error_array[i]=error_array[i-1]
 
-for i in range(0, COUNT, 1):
+for i in range(0, MAX_COUNT, 1):
     epoch_array[i] = float(epoch_array[i])
     error_array[i] = float(error_array[i])
 
